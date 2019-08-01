5cd8e99258afbb82b6afa14e469b3d3586adf822
Hieu <tranhieu956230@gmail.com>
2019-08-01 15:10:29 +0700

    login test

--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,10 +1,62 @@
 import React from "react";
-import { shallow } from "enzyme";
-import Login from "./index";
+import { mount } from "enzyme";
+import { BrowserRouter, Route } from "react-router-dom";
+import { login } from "services";
+
+import Login from "./";
+
+jest.mock("services", () => ({
+  login: jest.fn()
+}));
 
 describe("Test Login Component", () => {
-  it("It should render without errors", () => {
-    const wrapper = shallow(<Login />);
+  let wrapper;
+  beforeEach(() => {
+    wrapper = mount(
+      <BrowserRouter>
+        <Route render={props => <Login {...props} />} />
+      </BrowserRouter>
+    );
+  });
+
+  afterEach(() => {
+    login.mockReset();
+    wrapper.unmount();
+  });
+
+  it("Should render without errors", () => {
     expect(wrapper).toBeDefined();
+  });
+
+  it("Should render errors message", () => {
+    const username = wrapper.find(".input__field").at(0);
+    username.simulate("change", { target: { value: "tranhieu" } });
+    wrapper.find(".login__form").simulate("submit");
+    expect(login).toHaveBeenCalledTimes(0);
+  });
+
+  it("Should call api with arguments", async done => {
+    const username = wrapper.find(".input__field").at(0);
+    const password = wrapper.find(".input__field").at(1);
+
+    username.simulate("change", { target: { value: "username" } });
+    password.simulate("change", { target: { value: "password" } });
+
+    login.mockReturnValueOnce(
+      new Promise(resolve => {
+        resolve({
+          code: "SUCCESS",
+          result: {
+            access_token: "token"
+          }
+        });
+      })
+    );
+
+    wrapper.find(".login__form").simulate("submit");
+    setTimeout(() => {
+      expect(login).toBeCalledWith("username", "password");
+      done();
+    });
   });
 });
