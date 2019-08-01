2f781883aca6146eca8c8de62e8a50f6a32eb05a
Hieu <tranhieu956230@gmail.com>
2019-08-01 14:51:14 +0700

    Home test

--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,10 +1,37 @@
 import React from "react";
-import { shallow } from "enzyme";
-import Home from "./index";
+import { mount } from "enzyme";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Home from "./";
+import { getUserInfo } from "services";
+
+jest.mock("services", () => ({
+  getUserInfo: jest.fn()
+}));
 
 describe("Test Home Component", () => {
-  it("It should render without errors", () => {
-    const wrapper = shallow(<Home />);
-    expect(wrapper).toBeDefined();
+  it("Should get user info and render username", async done => {
+    getUserInfo.mockImplementationOnce(() => {
+      return new Promise(resolve => {
+        resolve({
+          code: "SUCCESS",
+          result: {
+            username: "Hieu"
+          }
+        });
+      });
+    });
+
+    const wrapper = mount(
+      <BrowserRouter>
+        <Route render={props => <Home {...props} />} />
+      </BrowserRouter>
+    );
+
+    setTimeout(() => {
+      const title = wrapper.find("h1").text();
+      expect(title).toEqual("Welcome Hieu");
+      done();
+    });
   });
 });
